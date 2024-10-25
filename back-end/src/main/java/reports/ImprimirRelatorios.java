87318eb685367d6579e6893f4734a6a4ae903a17
maurilio <mauriliomg8@gmail.com>
2024-10-24 23:55:59 -0300

    Criacao das Classes de Relatorios

---
+++ back-end/src/main/java/reports/ImprimirRelatorios.java
@@ -0,0 +1,239 @@
+package reports;
+
+import java.util.List;
+import java.util.Scanner;
+import model.Especialidade;
+import model.Historico;
+import model.Hospital;
+import model.Medico;
+import model.Paciente;
+import utils.Config;
+
+public class ImprimirRelatorios {
+    private final Scanner scanner = new Scanner(System.in);
+
+    Relatorios relatorios = new Relatorios();
+
+    public void imprimirRelatorioMedicos() {
+        List<Medico> medicos = relatorios.getRelatorioMedicos();
+        String linha = "+-------+----------------------+-----------------+";
+        if (medicos.isEmpty()) {
+            System.out.println("Nenhum medico encontrado.");
+        } else {
+
+            String formatarCabecalho = "| %-5s | %-20s | %-15s |%n";
+            String formatarLinha = "| %-5d | %-20s | %-15s |%n";
+
+            System.out.println("Relatorio de Medicos:");
+            System.out.println(linha);
+            System.out.format(formatarCabecalho, "ID", "Nome", "Conselho");
+            System.out.println(linha);
+
+            for (Medico medico : medicos) {
+                System.out.format(formatarLinha, medico.getIdMedico(), medico.getNomeMedico(), medico.getConselho());
+            }
+
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+
+        }
+    }
+
+    public void imprimirRelatorioEspecialidades() {
+        List<Especialidade> especialidades = relatorios.getRelatorioEspecialidade();
+        String linha = "+-------+----------------------+";
+        if (especialidades.isEmpty()) {
+            System.out.println("Nenhuma especialidade encontrada.");
+        } else {
+            String formatarCabecalho = "| %-5s | %-20s |%n";
+            String formatarLinha = "| %-5d | %-20s |%n";
+
+            System.out.println("Relatorio de Especialidades:");
+            System.out.println(linha);
+            System.out.format(formatarCabecalho, "ID", "Nome");
+            System.out.println(linha);
+
+            for (Especialidade especialidade : especialidades) {
+                System.out.format(formatarLinha, especialidade.getIdEspecialidade(),
+                        especialidade.getNomeEspecialidade());
+            }
+
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+
+        }
+    }
+
+    public void imprimirRelatorioHospitais() {
+        List<Hospital> hospitais = relatorios.getRelatorioHospitais();
+        String linha = "+-------+--------------------------------+-----------------+-----------------+--------------+--------------------------------+----------------------+-------------------------+---------------+";
+        if (hospitais.isEmpty()) {
+            System.out.println("Nenhum hospital encontrado.");
+        } else {
+            String formatarCabecalho = "| %-5s | %-30s | %-15s | %-15s | %-12s | %-30s | %-20s | %-23s | %-13s |%n";
+            String formatarLinha = "| %-5s | %-30s | %-15s | %-15s | %-12s | %-30s | %-20s | %-23s | %-13s |%n";
+
+            System.out.println("Relatorio de Hospitais:");
+            System.out.println(linha);
+
+            System.out.format(formatarCabecalho, "ID", "Razao Social", "CNPJ", "Categoria", "Telefone", "Email",
+                    "Logradouro", "Bairro", "Cidade");
+            System.out.println(linha);
+            for (Hospital hospital : hospitais) {
+                System.out.format(formatarLinha,
+                        hospital.getIdHospital(),
+                        hospital.getRazaoSocial(),
+                        hospital.getCnpj(),
+                        hospital.getCategoria(),
+                        hospital.getTelefone(),
+                        hospital.getEmail(),
+                        hospital.getIdEndereco().getLogradouro(),
+                        hospital.getIdEndereco().getBairro(),
+                        hospital.getIdEndereco().getCidade());
+            }
+
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+        }
+    }
+
+    public void imprimirRelatorioPacientes() {
+        List<Paciente> pacientes = relatorios.getRelatorioPacientes();
+        String linha = "+-------+--------------------------------+-----------------+-----------------+--------------+--------------------------------+----------------------+-------------------------+---------------+";
+        if (pacientes.isEmpty()) {
+            System.out.println("Nenhum paciente encontrado.");
+        } else {
+            String formatarCabecalho = "| %-5s | %-30s | %-15s | %-15s | %-12s | %-30s | %-20s | %-23s | %-13s |%n";
+            String formatarLinha = "| %-5s | %-30s | %-15s | %-15s | %-12s | %-30s | %-20s | %-23s | %-13s |%n";
+
+            System.out.println("Relatorio de Hospitais:");
+            System.out.println(linha);
+            System.out.format(formatarCabecalho, "ID", "Nome Paciente", "Dt Nascimento", "CPF", "Telefone", "Email",
+                    "Logradouro", "Bairro", "Cidade");
+            System.out.println(linha);
+            for (Paciente paciente : pacientes) {
+                System.out.format(formatarLinha,
+                        paciente.getIdPaciente(),
+                        paciente.getNomePaciente(),
+                        paciente.getDataNascimento(),
+                        paciente.getCpf(),
+                        paciente.getTelefone(),
+                        paciente.getEmail(),
+                        paciente.getIdEndereco().getLogradouro(),
+                        paciente.getIdEndereco().getBairro(),
+                        paciente.getIdEndereco().getCidade());
+            }
+
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+        }
+    }
+
+    public void imprimirRelatorioHistorico() {
+        List<Historico> historicos = relatorios.getRelatorioHistoricos();
+        String linha = "+-------+--------------+---------------------------+---------------------------+----------------------+-------------------------------------+-----------------+--------------------------------------------------------------+";
+        if (historicos.isEmpty()) {
+            System.out.println("Nenhum registro encontrado.");
+        } else {
+
+            String formatarCabecalho = "| %-5s | %-12s | %-25s | %-25s | %-20s | %-35s | %-15s | %-60s |%n";
+            String formatarLinha = "| %-5s | %-12s | %-25s | %-25s | %-20s | %-35s | %-15s | %-60s |%n";
+
+            System.out.println("Relatorio de Hospitais:");
+
+            System.out.println(linha);
+
+            System.out.format(formatarCabecalho, "ID", "Dt Consulta", "Nome Paciente", "Nome do Medico",
+                    "Especialidade",
+                    "Hospital", "Categoria", "Observação");
+
+            System.out.println(linha);
+
+            for (Historico historico : historicos) {
+                System.out.format(formatarLinha,
+                        historico.getIdHistorico(),
+                        historico.getDataConsulta(),
+                        historico.getIdPaciente().getNomePaciente(),
+                        historico.getIdMedico().getNomeMedico(),
+                        historico.getIdEspecialidade().getNomeEspecialidade(),
+                        historico.getIdHospital().getRazaoSocial(),
+                        historico.getIdHospital().getCategoria(),
+                        historico.getObservacao());
+            }
+
+            System.out.println(linha);
+
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+        }
+    }
+
+    public void imprimirRelatorioEspecialidadeMedicos() {
+        List<String[]> relatorio = relatorios.getRelatorioEspecialidadeMedicos();
+        String linha = "+--------------------------------+--------------------------------+";
+        if (relatorio.isEmpty()) {
+            System.out.println("Nenhum dado encontrado.");
+        } else {
+            String formatarCabecalho = "| %-30s | %-30s |%n";
+            String formatarLinha = "| %-30s | %-30s |%n";
+
+            System.out.println("Relatorio de Medicos e Especialidades:");
+            System.out.println(linha);
+
+            System.out.format(formatarCabecalho, "Nome do Medico", "Nome da Especialidade");
+
+            System.out.println(linha);
+
+            for (String[] registro : relatorio) {
+                System.out.format(formatarLinha, registro[0], registro[1]);
+            }
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+        }
+    }
+
+    public void imprimirRelatorioHospitalMedicos() {
+        List<String[]> relatorio = relatorios.getRelatorioHospitalMedicos();
+        String linha = "+--------------------------------+-----------------+--------------------------------+----------------------+--------------------------------+";
+        if (relatorio.isEmpty()) {
+            System.out.println("Nenhum dado encontrado.");
+        } else {
+            String formatarCabecalho = "| %-30s | %-15s | %-30s | %-20s | %-30s |%n";
+            String formatarLinha = "| %-30s | %-15s | %-30s | %-20s | %-30s |%n";
+
+            System.out.println("Relatorio de Medicos, Hospitais e Especialidades:");
+            System.out.println(linha);
+            System.out.format(formatarCabecalho, "Nome do Medico", "Conselho", "Hospital", "Categoria",
+                    "Especialidade");
+            System.out.println(linha);
+
+            for (String[] registro : relatorio) {
+                System.out.format(formatarLinha, registro[0], registro[1], registro[2], registro[3], registro[4]);
+            }
+
+            System.out.println(linha);
+            System.out.println("Aperte enter para sair");
+
+            scanner.nextLine();
+            Config.limparConsole(2);
+        }
+    }
+
+}
---
+++ back-end/src/main/java/reports/Relatorios.java
@@ -0,0 +1,267 @@
+package reports;
+
+import conexion.DatabaseConfig;
+import controller.*;
+import model.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Relatorios {
+
+    private String queryRelatorioEspecialidades;
+    private String queryRelatorioHospitais;
+    private String queryRelatorioPacientes;
+    private String queryRelatorioMedicos;
+    private String queryRelatorioHistoricos;
+    private String queryRelatorioEspecialidadeMedicos;
+    private String queryRelatorioHospitaisMedicos;
+
+    public Relatorios() {
+        queryRelatorioEspecialidades = lerArquivoSQL("sql/listar_especialidades.sql");
+        queryRelatorioHospitais = lerArquivoSQL("sql/listar_hospitais.sql");
+        queryRelatorioPacientes = lerArquivoSQL("sql/listar_pacientes.sql");
+        queryRelatorioMedicos = lerArquivoSQL("sql/listar_medicos.sql");
+        queryRelatorioHistoricos = lerArquivoSQL("sql/listar_historicos.sql");
+        queryRelatorioEspecialidadeMedicos = lerArquivoSQL("sql/listar_medicos_especialidade.sql");
+        queryRelatorioHospitaisMedicos = lerArquivoSQL("sql/listar_medicos_hospitais.sql");
+    }
+
+    private String lerArquivoSQL(String caminhoArquivo) {
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(caminhoArquivo);
+            if (inputStream != null) {
+                try (BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+                }
+            } else {
+                System.err.println("Arquivo SQL não encontrado: " + caminhoArquivo);
+                return null;
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao ler o arquivo SQL: " + caminhoArquivo + " - " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Especialidade> getRelatorioEspecialidade() {
+        List<Especialidade> especialidades = new ArrayList<>();
+        if (queryRelatorioEspecialidades == null) {
+            System.err.println("A consulta SQL está nula.");
+            return especialidades;
+        }
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement especialidade = conexao.createStatement();
+                ResultSet resultado = especialidade.executeQuery(queryRelatorioEspecialidades)) {
+
+            while (resultado.next()) {
+                int codigo = resultado.getInt("ID_ESPECIALIDADE");
+                String nome = resultado.getString("NOME_ESPECIALIDADE");
+                especialidades.add(new Especialidade(codigo, nome));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return especialidades;
+    }
+
+    public List<Medico> getRelatorioMedicos() {
+        List<Medico> medicos = new ArrayList<>();
+        if (queryRelatorioMedicos == null) {
+            System.err.println("A consulta SQL está nula.");
+            return medicos;
+        }
+
+        try (Connection conn = DatabaseConfig.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(queryRelatorioMedicos)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("ID_MEDICO");
+                String nome = rs.getString("NOME");
+                String conselho = rs.getString("CONSELHO");
+                medicos.add(new Medico(id, nome, conselho));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return medicos;
+    }
+
+    public List<Hospital> getRelatorioHospitais() {
+        ArrayList<Hospital> hospitais = new ArrayList<>();
+        if (queryRelatorioHospitais == null) {
+            System.err.println("A consulta SQL está nula.");
+            return hospitais;
+        }
+        EnderecoController enderecoController = new EnderecoController();
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement stmt = conexao.createStatement();
+                ResultSet resultado = stmt.executeQuery(queryRelatorioHospitais)) {
+
+            while (resultado.next()) {
+                int idHospital = resultado.getInt("ID_HOSPITAL");
+                String razaoSocial = resultado.getString("RAZAO_SOCIAL");
+                String cnpj = resultado.getString("CNPJ");
+                String email = resultado.getString("EMAIL");
+                String telefone = resultado.getString("TELEFONE");
+                String categoria = resultado.getString("CATEGORIA");
+                int idEndereco = resultado.getInt("e.ID_ENDERECO");
+
+                Endereco endereco = enderecoController.buscarEnderecoPorCodigo(idEndereco);
+                Hospital hospital = new Hospital(idHospital, razaoSocial, cnpj, email, telefone, categoria,
+                        endereco);
+
+                hospitais.add(hospital);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return hospitais;
+    }
+
+    public List<Paciente> getRelatorioPacientes() {
+        ArrayList<Paciente> pacientes = new ArrayList<>();
+
+        if (queryRelatorioPacientes == null) {
+            System.err.println("A consulta SQL está nula.");
+            return pacientes;
+        }
+        EnderecoController enderecoController = new EnderecoController();
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement stmt = conexao.createStatement();
+                ResultSet resultado = stmt.executeQuery(queryRelatorioPacientes)) {
+
+            while (resultado.next()) {
+                int idPaciente = resultado.getInt("ID_PACIENTE");
+                String nomePaciente = resultado.getString("NOME");
+                String email = resultado.getString("EMAIL");
+                String telefone = resultado.getString("TELEFONE");
+                String dataNascimento = resultado.getString("DATA_NASCIMENTO");
+                String cpf = resultado.getString("CPF");
+                int idEndereco = resultado.getInt("e.ID_ENDERECO");
+
+                Endereco endereco = enderecoController.buscarEnderecoPorCodigo(idEndereco);
+                Paciente paciente = new Paciente(idPaciente, nomePaciente, dataNascimento, email, telefone, cpf,
+                        endereco);
+
+                pacientes.add(paciente);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return pacientes;
+    }
+
+    public List<Historico> getRelatorioHistoricos() {
+        ArrayList<Historico> historicos = new ArrayList<>();
+        PacienteController pacienteController = new PacienteController();
+        HospitalController hospitalController = new HospitalController();
+        EspecialidadeController especialidadeController = new EspecialidadeController();
+        MedicoController medicoController = new MedicoController();
+
+        if (queryRelatorioHistoricos == null) {
+            System.err.println("A consulta SQL está nula.");
+            return historicos;
+        }
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement stmt = conexao.createStatement();
+                ResultSet resultado = stmt.executeQuery(queryRelatorioHistoricos)) {
+
+            while (resultado.next()) {
+                int idHistorico = resultado.getInt("ID_HISTORICO");
+                String dataConsulta = resultado.getString("DATA_CONSULTA");
+                String observacao = resultado.getString("OBSERVACAO");
+                int idPaciente = resultado.getInt("h.ID_PACIENTE");
+                int idHospital = resultado.getInt("h.ID_HOSPITAL");
+                int idEspecialidade = resultado.getInt("h.ID_ESPECIALIDADE");
+                int idMedico = resultado.getInt("h.ID_MEDICO");
+
+                Paciente paciente = pacienteController.buscarPorCodigoPaciente(idPaciente);
+                Hospital hospital = hospitalController.buscarPorCodigoHospital(idHospital);
+                Especialidade especialidade = especialidadeController.buscarPorCodigoEspecialidade(idEspecialidade);
+                Medico medico = medicoController.buscarPorCodigoMedico(idMedico);
+
+                Historico historico = new Historico(idHistorico, dataConsulta, observacao, paciente, hospital, medico,
+                        especialidade);
+
+                historicos.add(historico);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return historicos;
+    }
+
+    public List<String[]> getRelatorioEspecialidadeMedicos() {
+        List<String[]> relatorio = new ArrayList<>();
+        if (queryRelatorioEspecialidadeMedicos == null) {
+            System.err.println("A consulta SQL está nula.");
+            return relatorio;
+        }
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement stmt = conexao.createStatement();
+                ResultSet resultado = stmt.executeQuery(queryRelatorioEspecialidadeMedicos)) {
+
+            while (resultado.next()) {
+                String nomeMedico = resultado.getString("NOME");
+                String nomeEspecialidade = resultado.getString("NOME_ESPECIALIDADE");
+                relatorio.add(new String[] { nomeMedico, nomeEspecialidade });
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return relatorio;
+    }
+
+    public List<String[]> getRelatorioHospitalMedicos() {
+        List<String[]> relatorio = new ArrayList<>();
+        if (queryRelatorioHospitaisMedicos == null) {
+            System.err.println("A consulta SQL está nula.");
+            return relatorio;
+        }
+
+        try (Connection conexao = DatabaseConfig.getConnection();
+                Statement stmt = conexao.createStatement();
+                ResultSet resultado = stmt.executeQuery(queryRelatorioHospitaisMedicos)) {
+
+            while (resultado.next()) {
+                String nomeMedico = resultado.getString("NOME");
+                String conselho = resultado.getString("CONSELHO");
+                String razaoSocial = resultado.getString("RAZAO_SOCIAL");
+                String categoria = resultado.getString("CATEGORIA");
+                String nomeEspecialidade = resultado.getString("NOME_ESPECIALIDADE");
+                relatorio.add(new String[] { nomeMedico, conselho, razaoSocial, categoria, nomeEspecialidade });
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return relatorio;
+    }
+
+}
